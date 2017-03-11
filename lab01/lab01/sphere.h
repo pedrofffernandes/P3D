6dd8f8a188e70f80014df2069b803c932c1a0fa2
Pedro Fernandes <jelkzul@gmail.com>
2017-03-11 17:26:13 +0000

    Add comment

--- lab01/lab01/sphere.h
+++ lab01/lab01/sphere.h
@@ -9,7 +9,9 @@ class Sphere : public Obj
 public:
 	Sphere(Vect* position, float r, Material * mat);
 	~Sphere();
-
+	//Returns Ray distance to "this"
+	//Returns 0.0f if doesn't intersect
+	//Returns 0.0f if origin is in surface
 	float intersect(Ray * ray);
 };
 
